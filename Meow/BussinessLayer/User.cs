d5d4fc2091dcf54fdb981f2375bffb1743d1aeed
icko <57589158+itcaka50@users.noreply.github.com>
2023-11-10 18:04:49 +0200

    business layer user pochti

---
+++ Meow/BussinessLayer/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace BussinessLayer
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3), Required]
+        public string Name { get; set; }
+
+        [Required, MinLength(7)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        [Required, Range(1, 101)]
+        public int Age { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        private User()
+        {
+
+        }
+
+        public User(int id, string name, string pass, string adress, int age, string email, string phone)
+        {
+            Id = id;
+            Name = name;
+            Password = pass;
+            Adress = adress;
+            Age = age;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ColetoAuto
 Proekt Cvqtko Medkov Noshtnata Ptica na Bulgaria Koev, Lucho, Ico
+
+TABLICI:
+User(id, names, password, address, age, email, phone), Admin(redactira listings)
+Cars(id, marka, model, hp, price, probeg, godina, desc) foreign key marka, foreign key model
+Flight(id, marka, model, price, desc, trust) foreign key marka, foreign key model
+Lodki(id, marka, model, price, hp, desc) foreign key marka, foreign key model
+Chasti(cena, marka, model) foreign key marka, foreign key model
+Marka
+Model
+
+WEBSITE:
+LOG PAGE
+MAIN PAGE
+BUY PAGE
+RESULTS PAGE
+ADMIN PAGE
