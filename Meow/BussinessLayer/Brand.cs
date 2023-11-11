84d58ccc53b4897c6d589bdf120bea68f67f2278
Hristp Atanasov <itcaka50@abv.bg>
2023-11-11 19:15:16 +0200

    Brand and model tablici

---
+++ Meow/BussinessLayer/Brand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer
+{
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BrandId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        [Required]
+        public ICollection<Model> Models { get; set; }
+
+        private Brand()
+        {
+            Models = new List<Model>();
+        }
+        
+        public Brand(int brandId, ICollection<Model> models)
+        {
+            this.BrandId = brandId;
+            this.Models = models;
+        }
+    }
+}
---
+++ Meow/BussinessLayer/Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BussinessLayer
+{
+    public class Model
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ModelId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey("Brand")]
+        public int BrandIdF { get; set; }
+
+        public Brand brand { get; set; }
+
+        private Model() {}
+
+        public Model(int modelId, string name, int brandId)
+        {
+            ModelId = modelId;
+            Name = name;
+            BrandIdF = brandId;
+        }
+
+    }
+}
