b73f1e6af0d55e02a3ee5eee528a25f47541e8e8
Hristp Atanasov <itcaka50@abv.bg>
2023-11-11 19:25:36 +0200

    DBcontext

---
+++ Meow/DataLayer/MeowDBContext.cs
@@ -0,0 +1,49 @@
+﻿using BussinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class MeowDbContext : DbContext
+    {
+        public MeowDbContext() : base()
+        {
+
+        }
+
+        public MeowDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\DEFAULTSQLSERVER\\SQLEXPRESS;Database=MeowDB;Trusted_Connection=True;Encryption=False");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Aircraft> Aircrafts { get; set; }
+
+        public DbSet<Boat> Boats { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Model> Models { get; set; }
+    }
+}
