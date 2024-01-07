2992e2fca756e516f3ee2fa5560c154b79a642f7
TestGit <lachezar855@gmail.com>
2024-01-07 22:44:09 +0200

    a

---
+++ Meow/TestingLayer/SetupFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestingLayer
+{
+    [SetUpFixture]
+    public static class SetupFixture
+    {
+        public static MeowDbContext dbContext;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            dbContext = new MeowDbContext(builder.Options);
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            dbContext.Dispose();
+        }
+    }
+}
