a27234bf9ffb36a77f8fbc4521198aa9ab0b7fb8
Hristp Atanasov <itcaka50@abv.bg>
2024-02-25 18:37:48 +0200

    controllers

---
+++ Meow/MVC/Controllers/AircraftController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BussinessLayer;
+using DataLayer;
+
+namespace MVC.Controllers
+{
+    public class AircraftController : Controller
+    {
+        private readonly MeowDbContext _context;
+
+        public AircraftController(MeowDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Aircraft
+        public async Task<IActionResult> Index()
+        {
+            var meowDbContext = _context.Aircrafts.Include(a => a.Brand).Include(a => a.Model);
+            return View(await meowDbContext.ToListAsync());
+        }
+
+        // GET: Aircraft/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Aircrafts == null)
+            {
+                return NotFound();
+            }
+
+            var aircraft = await _context.Aircrafts
+                .Include(a => a.Brand)
+                .Include(a => a.Model)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+
+            return View(aircraft);
+        }
+
+        // GET: Aircraft/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandIdF"] = new SelectList(_context.Brands, "BrandId", "Name");
+            ViewData["ModelIdF"] = new SelectList(_context.Models, "ModelId", "Name");
+            return View();
+        }
+
+        // POST: Aircraft/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Price,Thrust,Description,BrandIdF,ModelIdF")] Aircraft aircraft)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aircraft);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandIdF"] = new SelectList(_context.Brands, "BrandId", "Name", aircraft.BrandIdF);
+            ViewData["ModelIdF"] = new SelectList(_context.Models, "ModelId", "Name", aircraft.ModelIdF);
+            return View(aircraft);
+        }
+
+        // GET: Aircraft/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Aircrafts == null)
+            {
+                return NotFound();
+            }
+
+            var aircraft = await _context.Aircrafts.FindAsync(id);
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandIdF"] = new SelectList(_context.Brands, "BrandId", "Name", aircraft.BrandIdF);
+            ViewData["ModelIdF"] = new SelectList(_context.Models, "ModelId", "Name", aircraft.ModelIdF);
+            return View(aircraft);
+        }
+
+        // POST: Aircraft/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Price,Thrust,Description,BrandIdF,ModelIdF")] Aircraft aircraft)
+        {
+            if (id != aircraft.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aircraft);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AircraftExists(aircraft.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandIdF"] = new SelectList(_context.Brands, "BrandId", "Name", aircraft.BrandIdF);
+            ViewData["ModelIdF"] = new SelectList(_context.Models, "ModelId", "Name", aircraft.ModelIdF);
+            return View(aircraft);
+        }
+
+        // GET: Aircraft/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var aircraft = await _context.Aircrafts
+                .Include(a => a.Brand)
+                .Include(a => a.Model)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+
+            return View(aircraft);
+        }
+
+        // POST: Aircraft/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'MeowDbContext.Aircrafts'  is null.");
+            }
+            var aircraft = await _context.Users.FindAsync(id);
+            if (aircraft != null)
+            {
+                _context.Users.Remove(aircraft);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AircraftExists(int id)
+        {
+          return (_context.Aircrafts?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Meow/MVC/Controllers/ModelsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BussinessLayer;
+using DataLayer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MVC.Controllers
+{
+    public class ModelsController : Controller
+    {
+        private readonly MeowDbContext _context;
+
+        public ModelsController(MeowDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Models
+        public async Task<IActionResult> Index()
+        {
+              return _context.Models != null ? 
+                          View(await _context.Models.ToListAsync()) :
+                          Problem("Entity set 'MeowDbContext.Models'  is null.");
+        }
+
+        // GET: Models/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Models == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Models
+                .FirstOrDefaultAsync(m => m.ModelId == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        // GET: Models/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Models/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ModelId,Name,BrandIdF")] Model model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: Models/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Models == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Models.FindAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+
+        // POST: Models/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ModelId,Name,BrandIdF")] Model model)
+        {
+            if (id != model.ModelId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModelExists(model.ModelId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: Models/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Models == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Models
+                .FirstOrDefaultAsync(m => m.ModelId == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        // POST: Models/Delete/5
+        [Authorize(Roles = "Administrator")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Models == null)
+            {
+                return Problem("Entity set 'MeowDbContext.Models'  is null.");
+            }
+            var model = await _context.Models.FindAsync(id);
+            if (model != null)
+            {
+                _context.Models.Remove(model);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModelExists(int id)
+        {
+          return (_context.Models?.Any(e => e.ModelId == id)).GetValueOrDefault();
+        }
+    }
+}
